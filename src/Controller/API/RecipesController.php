375845ac78e7f89ea0f964a38aaaf7db4806e174
Malky <jb@malky.dev>
2026-07-29 15:24:59 +0200

    feat(api): add versioned recipe endpoints

--- src/Controller/API/RecipesController.php
+++ src/Controller/API/RecipesController.php
@@ -15,61 +15,73 @@ class RecipesController extends AbstractController
 {
 
     #[Route(
-        "/api/recipes.{_format}",
-        requirements: ['_format' => 'json|xml|csv'],
-        defaults: ['_format' => 'json']
+        '/api/v1/recipes',
+        name: 'api_v1_recipes_index',
+        methods: ['GET'],
+        defaults: ['_format' => 'json'],
     )]
-    public function index(RecipeRepository $repository, Request $request, SerializerInterface $serializer): Response
-    {
-
-        $recipes = $repository->paginateRecipes($request->query->getInt('page', 1));
+    #[Route(
+        '/api/recipes.{_format}',
+        name: 'api_legacy_recipes_index',
+        methods: ['GET'],
+        requirements: ['_format' => 'json|xml|csv'],
+        defaults: ['_format' => 'json'],
+    )]
+    public function index(
+        RecipeRepository $repository,
+        Request $request,
+        SerializerInterface $serializer,
+    ): Response {
+        $recipes = $repository->paginateRecipes(
+            $request->query->getInt('page', 1),
+        );
 
         $format = $request->getRequestFormat();
 
         $data = $serializer->serialize($recipes, $format, [
-                'groups' => ['recipes.index']
+            'groups' => ['recipes.index'],
         ]);
 
-        $contentType = $this->getContentType($format);
-
-        return new Response($data, 200, ['Content-Type' => $contentType]);
-        
-        // return $this->json(
-        //     $repository->paginateRecipes($request->query->getInt('page', 1)), 
-        //     200, 
-        //     [], 
-        //     [
-        //         'groups' => ['recipes.index']
-        //     ]);
-
+        return new Response(
+            $data,
+            Response::HTTP_OK,
+            ['Content-Type' => $this->getContentType($format)],
+        );
     }
 
     #[Route(
-        "/api/recipes/{id}.{_format}",
-        requirements: ['id' => Requirement::DIGITS, '_format' => 'json|xml|csv'],
-        defaults: ['_format' => 'json']
+        '/api/v1/recipes/{id}',
+        name: 'api_v1_recipes_show',
+        methods: ['GET'],
+        requirements: ['id' => Requirement::DIGITS],
+        defaults: ['_format' => 'json'],
     )]
-    public function show(Recipe $recipe, Request $request, SerializerInterface $serializer): Response
-    {
-
+    #[Route(
+        '/api/recipes/{id}.{_format}',
+        name: 'api_legacy_recipes_show',
+        methods: ['GET'],
+        requirements: [
+            'id' => Requirement::DIGITS,
+            '_format' => 'json|xml|csv',
+        ],
+        defaults: ['_format' => 'json'],
+    )]
+    public function show(
+        Recipe $recipe,
+        Request $request,
+        SerializerInterface $serializer,
+    ): Response {
         $format = $request->getRequestFormat();
 
         $data = $serializer->serialize($recipe, $format, [
-                'groups' => ['recipes.index', 'recipes.show']
+            'groups' => ['recipes.index', 'recipes.show'],
         ]);
 
-        $contentType = $this->getContentType($format);
-
-        return new Response($data, 200, ['Content-Type' => $contentType]);
-
-        // return $this->json(
-        //     $recipe, 
-        //     200, 
-        //     [], 
-        //     [
-        //         'groups' => ['recipes.index', 'recipes.show']
-        //     ]);
-
+        return new Response(
+            $data,
+            Response::HTTP_OK,
+            ['Content-Type' => $this->getContentType($format)],
+        );
     }
 
     private function getContentType(string $format): String
