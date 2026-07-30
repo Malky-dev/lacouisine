5ae6a18e89788ff1a837034bd780e6866c3b4c07
Malky <jb@malky.dev>
2026-07-30 16:36:37 +0200

    feat(auth): add hierarchical content permissions

--- src/Controller/Admin/CategoryController.php
+++ src/Controller/Admin/CategoryController.php
@@ -16,7 +16,7 @@ use Symfony\Component\Routing\Requirement\Requirement;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/admin/category/', name: 'admin.category.')]
-#[IsGranted('ROLE_USER')]
+#[IsGranted('ROLE_MANAGER')]
 class CategoryController extends AbstractController {
 
     
--- src/Controller/Admin/RecipeController.php
+++ src/Controller/Admin/RecipeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Recipe;
+use App\Entity\User;
 use App\Form\RecipeType;
 use App\Repository\RecipeRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -15,7 +16,7 @@ use Symfony\Component\Routing\Requirement\Requirement;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/admin/recettes/', name: 'admin.recipe.')]
-#[IsGranted('ROLE_USER')]
+#[IsGranted('ROLE_ADMIN')]
 final class RecipeController extends AbstractController
 {
     #[Route(name: 'index')]
@@ -36,6 +37,13 @@ final class RecipeController extends AbstractController
     {
 
         $recipe = new Recipe();
+        $user = $this->getUser();
+
+        if (!$user instanceof User) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $recipe->setCreatedBy($user);
 
         $form = $this->createForm(RecipeType::class, $recipe);
         $form->handleRequest($request);
